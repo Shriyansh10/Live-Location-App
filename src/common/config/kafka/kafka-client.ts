dcf2a5c2869b17b9b8e5db4a7305faab5791c149
Shriyansh007 <agarwalshriyansh007@gmail.com>
2026-05-01 23:06:29 +0530

    Configure Kafka client with environment variables for brokers and authentication

--- src/common/config/kafka/kafka-client.ts
+++ src/common/config/kafka/kafka-client.ts
@@ -1,6 +1,14 @@
 import { Kafka } from "kafkajs";
 
 export const kafkaClient = new Kafka({
-    clientId: 'shriyansh',
-    brokers: ['localhost:9092']
-})
+  clientId: "shriyansh",
+  brokers: [process.env.KAFKA_BROKER!],
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  sasl: {
+    mechanism: "plain",
+    username: process.env.KAFKA_USERNAME!,
+    password: process.env.KAFKA_PASSWORD!,
+  },
+});
